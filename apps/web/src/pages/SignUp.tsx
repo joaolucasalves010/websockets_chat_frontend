2a13e463eaf131a575cf896aaeca9a5e9cdd49ce
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-16 22:16:37 -0300

    feat: adicionando Footer na págna de SignUp

--- apps/web/src/pages/SignUp.tsx
+++ apps/web/src/pages/SignUp.tsx
@@ -1,12 +1,16 @@
 import SignUpComponent from "@/components/SignUpComponent"
+import Footer from "@/components/Footer"
 
 const SignUp = () => {
   return (
-    <main className="bg-zinc-100 min-h-screen flex justify-center items-center">
-      <section className="bg-white rounded-lg max-w-md">
-        <SignUpComponent />
-      </section>
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <main className="bg-zinc-100 flex flex-1 justify-center items-center">
+        <section className="bg-white rounded-lg max-w-md">
+          <SignUpComponent />
+        </section>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
