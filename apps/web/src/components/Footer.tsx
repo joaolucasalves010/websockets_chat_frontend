ac568e6cdedbad76bd4a10d9539fb76fd40d508e
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-16 22:16:18 -0300

    feat: adicionando links das redes sociais

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
-    <footer>
+    <footer className="flex items-center justify-between p-8 border-t-zinc-300 border text-sm gap-2">
       <p>Feito por <strong>João Lucas</strong></p>
+      <div className="flex items-center gap-2">
+        <a href="https://instagram.com/joaolucasalves010" className="hover:border-zinc-400 border rounded-full p-1" target="_blank"><FontAwesomeIcon className="md:text-2xl lg:text-2xl" icon={faInstagram} style={{color: "rgb(128, 128, 128)",}} /></a>
+        <a href="https://github.com/joaolucasalves010" className="hover:border-zinc-400 border rounded-full p-1" target="_blank"><FontAwesomeIcon className="md:text-2xl lg:text-2xl" icon={faGithub} style={{color: "rgb(128, 128, 128)",}} /></a>
+        <a href="https://www.linkedin.com/in/jo%C3%A3o-lucas-lima-alves-4b8004298/" className="hover:border-zinc-400 border rounded-full p-1" target="_blank"><FontAwesomeIcon className="md:text-2xl lg:text-2xl" icon={faLinkedin} style={{color: "rgb(128, 128, 128)",}} /></a>
+      </div>
     </footer>
   )
 }
