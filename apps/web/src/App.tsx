6516b2ac47dc48805fe7da02c74aa2cc7c498e45
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-19 20:54:10 -0300

    feat: adicionando rota de signin

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
 
 export function App() {
 
@@ -8,6 +9,7 @@ export function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<SignUp />} path="/signup"/>
+        <Route element={<SignIn />} path="/signin" />
       </Routes>
     </BrowserRouter>
   )
